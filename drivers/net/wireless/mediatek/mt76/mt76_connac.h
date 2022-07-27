b580afefd67dbcdbc0676be908b8476caa033dfe
Ben Greear <greearb@candelatech.com>
2022-07-27 15:39:05 -0700

    wifi: mt76: mt7915: add support for tx-overrides
    Allow setting fixied rate on transmit without using full testmode
    logic.
    
    The goal of tx-overrides is to allow receiver sensitivity and other
    types of testing.  Assumption is that the packets we care about
    are larger data frames, so exclude smaller packets and non
    data packets.
    
    Setting transmit count to '0' means transmit once, but flag
    the packet as no-ack.
    
    Allows adjusting the tx power from default value, from -8 to +8
    dbm.  Adjusting above 0 is probably not a good idea.
    
    The station wtbl logic to read rate-ctrl settings does not work when
    fixed rates are used.  So, read sgi settings from the txo configuration
    in this case.
    
    Signed-off-by: Ben Greear <greearb@candelatech.com>

--- drivers/net/wireless/mediatek/mt76/mt76_connac.h
+++ drivers/net/wireless/mediatek/mt76/mt76_connac.h
@@ -355,7 +355,7 @@ void mt76_connac2_mac_write_txwi(struct mt76_dev *dev, __le32 *txwi,
 				 struct ieee80211_key_conf *key, int pid,
 				 enum mt76_txq_id qid, u32 changed);
 bool mt76_connac2_mac_fill_txs(struct mt76_dev *dev, struct mt76_wcid *wcid,
-			       __le32 *txs_data);
+			       __le32 *txs_data, struct ieee80211_tx_info *info);
 bool mt76_connac2_mac_add_txs_skb(struct mt76_dev *dev, struct mt76_wcid *wcid,
 				  int pid, __le32 *txs_data);
 void mt76_connac2_mac_decode_he_radiotap(struct mt76_dev *dev,
